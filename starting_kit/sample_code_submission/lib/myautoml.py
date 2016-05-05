ab0d0a9a645e1a0c22b88705ed32fc294300e808
ngjunjie1991 <ngjunjie1991@gmail.com>
2016-05-05 00:01:15 -0700

    added myautoml.py

---
+++ starting_kit/sample_code_submission/lib/myautoml.py
@@ -0,0 +1,110 @@
+import numpy as np
+import scipy as sp
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis, QuadraticDiscriminantAnalysis
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.pipeline import Pipeline
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import chi2
+import operator
+import copy
+
+class MyAutoML:
+    '''
+    Constructor initializes instance variables:
+        self.label_num
+        self.target_num
+        self.task
+        self.metric
+    '''
+    def __init__(self, info, verbose=True, debug_mode=False, run_on_gpu=False):
+        self.label_num = info['label_num']
+        self.target_num = info['target_num']
+        self.task = info['task']
+        self.metric = info['metric']
+
+
+    def run_cycles(self, X, y, time_budget):
+        '''
+        Input: 
+            X - data matrix (train_num, feat_num)
+            y - target labels matrix (train_num, label_num)
+        Output: 
+            p - predictions (test_num, target_num)
+
+        While there is still time budget left, tune all 3 classifiers in each cycle predicts on best classifier (including ensemble). For each cycle, increase size of hyperparameters to tune.
+        '''
+
+    def train_DA(self, X, y, hp):
+        '''
+        Input: 
+            None
+            X - data matrix (train_num, feat_num)
+            y - target labels matrix (train_num, label_num)
+
+        Output: 
+            best_clf - best classifier trained (QDA/LDA)
+            best_score - CV score of best classifier
+
+        Find best DA classifier.
+        '''
+        return best_clf, best_score
+
+    def train_NN(self, X, y, hp):
+        '''
+        Input: 
+            hp - dictionary with hyperparameter name as keys and values as values (refer to format in sklearn.grid_search.GridSearchCV)
+
+            X - data matrix (train_num, feat_num)
+            y - target labels matrix (train_num, label_num)
+
+        Output: 
+            best_clf - best classifier trained with hp
+            best_score - CV score of best classifier
+
+        Tunes neural net with hp.    
+        '''
+        return best_clf, best_score
+
+    def train_RF(self, X, y, hp):
+        '''
+        Input: 
+            hp - dictionary with hyperparameter name as keys and values as values (refer to format in sklearn.grid_search.GridSearchCV)
+            example: {n_trees: [10, 100, 1000]}
+            X - data matrix (train_num, feat_num)
+            y - target labels matrix (train_num, label_num)
+
+        Output: 
+            best_clf - best classifier trained with hp
+            best_score - CV score of best classifier
+
+        Tunes random forest with hp.    
+        '''
+        return best_clf, best_score
+
+    def ensemble_CV(self, da, nn, rf, X, y):
+        '''
+        Input:
+            da - best da classifier
+            nn - best nn classifier
+            rf - best rf classifier
+            X - data matrix (train_num, feat_num)
+            y - target labels matrix (train_num, label_num)
+        Output:
+            score - CV score of ensemble classifier
+
+        Used to find out if ensemble method does better at CV then individual classifier.
+        '''
+        return score
+
+    def ensemble_predict(self, da, nn, rf, test):
+        '''
+        Input:
+            da - best da classifier
+            nn - best nn classifier
+            rf - best rf classifier
+            test - test matrix (test_num, feat_num)
+
+        Output:
+            pred - predictions (test_num, target_num)
+        '''
+        return pred
